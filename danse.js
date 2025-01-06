e7ae830495f039b14f9dcc8033b8b06d1a324019
Ambre Roffidal <maelle.roffidal@etu.univ-amu.fr>
2025-01-06 16:37:42 +0100

    fichiers finaux

--- danse.js
+++ danse.js
@@ -1,14 +1,12 @@
-const theme = document.querySelector('.theme');
+const theme = document.querySelector('.switch-theme');
 theme.addEventListener('click',()=>{
     const body = document.body;
     if(body.classList.contains('dark')){
         body.classList.add('light')
         body.classList.remove('dark')
-        theme.innerHTML="Dark"
     }
     else {
         body.classList.add('dark')
         body.classList.remove('light')
-        theme.innerHTML="Light"
     }
 })
